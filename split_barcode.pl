86fdf45b59310e648e912217a578920e1a9f33e8
Chen.guangji <31179245+ChenGuangji@users.noreply.github.com>
2020-02-11 20:21:15 +0800

    Update split_barcode.pl

--- split_barcode.pl
+++ split_barcode.pl
@@ -67,12 +67,12 @@ while(<IN2>){
             $n4 = 18 ;
         }elsif( $r2_length == 142 ){
             $n4 = 6 ;
-        }elsif( $r2_length == 126 ){
-            $n4 = 0 ;
-            $n5 = 0 ;
         }elsif( $r2_length == 130 ){
             $n2 = 0 ;
             $n4 = 0 ;
+        }elsif( $r2_length == 126 ){
+            $n4 = 0 ;
+            $n5 = 0 ;
         }else{
             print "Unknow read length of r2:$r2_length.\nPlease check the $ARGV[2] file!\n";
             exit(1);
